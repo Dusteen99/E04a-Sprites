83feb59c4631c19cff42d5af408f51a42569dbaa
duadkins <duadkins@iu.edu>
2019-09-23 16:34:07 -0400

    Need to finished main3

--- main3.py
+++ main3.py
@@ -33,11 +33,18 @@ class MyGame(arcade.Window):
 
 
     def update(self, delta_time):
-        pass
+        self.animal_list.draw()
 
 
     def on_mouse_motion(self, x, y, dx, dy):
-        pass
+        for a in self.animal_list:
+            print(a.animal_sprite.center_x)
+            print(a.animal_sprite.center_y)
+            a.animal_sprite.center_x = x
+            a.animal_sprite.center_y = y
+            print(a.animal_sprite.center_x)
+            print(a.animal_sprite.center_y)
+            
 
 def main():
     """ Main method """
